772e4d33d708a61ee61d7a048fc40a470d6351b9
mounvip1525 <mounvip1525@gmail.com>
2021-02-03 21:07:27 +0530

    cat button and footer added

--- src/components/Product.js
+++ src/components/Product.js
@@ -17,7 +17,7 @@ export default class Product extends Component {
                         <button className="cart-btn" disabled={inCart ? true : false }
                             onClick={()=>{console.log("added to the cart");}}>
                             { inCart ? 
-                                <p className="text-capitalize mb-0" disabled>{" "} in Cart</p>
+                                <p className="text-capitalize mb-0 cart-text" disabled>{" "} in Cart</p>
                                 :
                                 <i className="fa fa-cart-plus" />
                             }
@@ -35,6 +35,57 @@ export default class Product extends Component {
     }
 }
 
-const ProductWrapper=styled.div`
-background:transparent;
+const ProductWrapper = styled.div`
+  .card {
+    border-color: transparent;
+    transition: all 0.2s linear;
+  }
+  .cart-text{
+      font-size:0.85rem;
+  }
+  .card-footer {
+    background: transparent;
+    border-top: transparent;
+    transition: all 1s linear;
+  }
+  &:hover {
+    .card {
+      border: 0.04rem solid rgba(0, 0, 0, 0.2);
+      box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.2);
+    }
+    .card-footer {
+      background: rgba(247, 247, 247);
+    }
+  }
+  .img-container {
+    position: relative;
+    overflow: hidden;
+  }
+  .card-img-top {
+    transition: all 0.2s linear;
+  }
+  .img-container:hover .card-img-top {
+    transform: scale(1.1);
+    padding:5px;
+  }
+  .cart-btn {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    padding: 0.2rem 0.4rem;
+    background: var(--lightBlue);
+    border: none;
+    color: var(--mainWhite);
+    font-size: 1.4rem;
+    border-radius: 0.5rem 0 0 0;
+    transform: translate(100%, 100%);
+    transition: all 1s ease-in-out;
+    background-color: var(--purpleTwo);
+  }
+  .img-container:hover .cart-btn {
+    transform: translate(0, 0);
+  }
+  .cart-btn:hover {
+    cursor: pointer;
+  }
 `;
