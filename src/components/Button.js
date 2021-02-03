3c87987d691ba196cf08016743e16eecc6f9a33b
mounvip1525 <mounvip1525@gmail.com>
2021-02-03 13:56:08 +0530

    navbar completed

--- src/components/Button.js
+++ src/components/Button.js
@@ -10,11 +10,33 @@ border-radius:0.5rem;
 padding:0.2rem 0.5rem;
 cursor:pointer;
 margin:0.2rem 0.5rem 0.2rem 0;
+// transition:all 0.5s ease-in-out;
+&:hover{
+    color:var(--purpleTwo);
+    border:1px solid var(--purpleTwo);
+}
+&:focus{
+    outline:none;
+}
+`;
+export const ButtonContainer2=styled.button`
+text-transform:capitalize;
+font-size:1.2rem;
+background:transparent;
+border:none;
+color:var(--whiteOne);
+padding:0.2rem 0.5rem;
+cursor:pointer;
+margin:0.2rem 0.5rem 0.2rem 0;
 transition:all 0.5s ease-in-out;
 &:hover{
     color:white;
 }
 &:focus{
     outline:none;
+}
+a{
+    text-decoration:none;
+    color:var(--purpleTwo);
 }
 `;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,30 +2,73 @@ import React, { Component } from 'react';
 import logo from '../logo.svg';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import { ButtonContainer } from './Button';
+import { ButtonContainer, ButtonContainer2 } from './Button';
 
 export default class Navbar extends Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-expand-sm px-sm-3 bg-primary navbar-dark">
+                <h6 className="top">Welcome to Clothing store, an app developed using React</h6>
+                <NavWrapper className="navbar navbar-expand-sm px-sm-3  navbar-dark">
                     <Link to="/">
-                        <img src={logo} alt="Store" className="navbar-brand"></img>
+                        <img src={logo} alt="Store" className="navbar-brand mr-0" style={{marginTop:"-5px"}}></img>
                     </Link>
-                    <ul className="navbar-nav align-items-center">
+                    <ul className="navbar-nav align-items-center ">
                         <li className="nav-item">
-                            <Link to="/" className="nav-link"><h4>Clothing Store</h4></Link>
+                            <Link to="/" className="nav-link px-0"><h4 className="ml-10">Clothing Store</h4></Link>
                         </li>
                     </ul>
-                    <Link to="/cart" className="ml-auto">
+                    <div className="left-links">
+                    <ButtonContainer2>
+                    <Link to="/cart" className="left">
+                        <span className="mr-2"></span> cart44
+                    </Link> •
+                    <Link to="/cart" className="left">
+                        <span className="mr-2"></span> cart44
+                    </Link> •
+                    <Link to="/cart" className="left">
+                        <span className="mr-2"></span> cart44
+                    </Link> •
+                    <Link to="/cart" className="left">
+                        <span className="mr-2"></span> cart44
+                    </Link> •
+                    </ButtonContainer2>
+                    <Link to="/cart" className="ml-0">
                         <ButtonContainer>
                             <span className="mr-2"><i className="fa fa-cart-plus" /></span> cart
                         </ButtonContainer>
                     </Link>
-                </nav>
+                    </div>
+                </NavWrapper>
+                <div className="Nav2">
+                    One stop for amazing dresses,shirts,pants and many more! Enjoy Shopping
+                </div>
             </div>
         )
     }
 }
+
+const NavWrapper=styled.nav`
+background:var(--purpleOne);
+margin-top:10px;
+.nav-link{
+    color:var(--whiteOne) !important;
+    font-size:1.3rem;
+}
+h5{
+    color:var(--whiteOne) !important;
+}
+.left-links{
+    display:flex;
+    flex-direction:row;
+    margin-left:auto;
+}
+.left{
+    margin-right:1vw;
+}
+.right{
+    margin-left:1vw;
+}
+`
 
 
