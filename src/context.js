5f3fb8c9a64cef1952bc7041889d51de040e4a27
mounvip1525 <mounvip1525@gmail.com>
2021-02-04 18:59:54 +0530

    add to cart functionality added

--- src/context.js
+++ src/context.js
@@ -9,7 +9,8 @@ class ProductProvider extends Component {
     state={
         // products:storeProducts,
         products:[],
-        detailProduct:detailProduct
+        detailProduct:detailProduct,
+        cart:[]
     }
     componentDidMount(){
         this.setProducts();
@@ -39,7 +40,17 @@ class ProductProvider extends Component {
         })
     }
     addToCart=(id)=>{
-        console.log(`add to cart.id is ${id}`);
+        // console.log(`add to cart.id is ${id}`);
+        let tempProducts=[...this.state.products];
+        const index=tempProducts.indexOf(this.getItem(id));
+        const product=tempProducts[index];
+        product.inCart=true;
+        product.count=1;
+        const price=product.price;
+        product.total=price;
+        this.setState(()=>{
+            return{ products:tempProducts, cart:[...this.state.cart,product] };
+        },()=>{console.log(this.state)});
     }
     render() {
         return (
