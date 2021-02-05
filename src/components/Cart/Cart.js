63b7b006967a2107942394c3566c304e7d00702f
mounvip1525 <mounvip1525@gmail.com>
2021-02-05 14:58:56 +0530

    cart created

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react'
+import { ProductConsumer } from '../../context';
 import Title from '../Title';
 import CartColumns from './CartColumns';
-import EmptyCart from './EmptyCart';
+import CartList from './CartList';
 
 export default class Cart extends Component {
     render() {
         return (
-            <div>
-                <Title name="your" title="cart" />
-                <CartColumns />
-                <EmptyCart />
-            </div>
+            <section>
+                <ProductConsumer>
+                    {value=>{
+                        const {cart}=value;
+                        if(cart.length>0){
+                            return(
+                              <React.Fragment> 
+                                <Title name="your" title="cart" />
+                                <CartColumns />
+                                <CartList value={value}/>
+                              </React.Fragment> 
+                                )
+                        } else {
+                            return(<Title name="your cart is currently" title="empty" />)
+                        }
+                    }}
+                </ProductConsumer>
+            </section>
         )
     }
 }
