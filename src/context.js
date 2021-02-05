cd138c8a6618a683281e6d5adae7289019deae98
mounvip1525 <mounvip1525@gmail.com>
2021-02-05 16:09:14 +0530

    individual item deletion added

--- src/context.js
+++ src/context.js
@@ -64,6 +64,20 @@ class ProductProvider extends Component {
         console.log("decrement method");
     }
     removeItem=id=>{
+        let tempProducts=[...this.state.products];
+        let tempCart=[...this.state.cart];
+        tempCart=tempCart.filter(item=>item.id !== id);
+        const index=tempProducts.indexOf(this.getItem(id));
+        let removedProduct=tempProducts[index];
+        removedProduct.inCart=false;
+        removedProduct.count=0;
+        removedProduct.total=0;
+        this.setState(()=>{
+            return {
+                cart:[...tempCart],
+                products:[...tempProducts]
+            }
+        },()=>{this.computeTotals()})
         console.log("item removed from cart");
     }
     clearCart=()=>{
