a4304e84324959ef6d2361c8b77a539f8246960e
mounvip1525 <mounvip1525@gmail.com>
2021-02-05 15:14:15 +0530

    cartTotals component added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,6 +3,7 @@ import { ProductConsumer } from '../../context';
 import Title from '../Title';
 import CartColumns from './CartColumns';
 import CartList from './CartList';
+import CartTotals from './CartTotals';
 
 export default class Cart extends Component {
     render() {
@@ -17,6 +18,7 @@ export default class Cart extends Component {
                                 <Title name="your" title="cart" />
                                 <CartColumns />
                                 <CartList value={value}/>
+                                <CartTotals value={value} />
                               </React.Fragment> 
                                 )
                         } else {
