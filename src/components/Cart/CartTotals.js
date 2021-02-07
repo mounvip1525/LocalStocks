263adde96a0f5e94d5ed18dd9b6885a541a02db6
mounvip1525 <mounvip1525@gmail.com>
2021-02-07 22:12:16 +0530

    bil css modified

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -6,21 +6,24 @@ export default function CartTotals({value,history}) {
     const { cartSubTotal , cartTax , cartTotal , clearCart }=value;
     return (
         <div className="container">
-            <div className="row">
-                <div className="col-10 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-capitalize text-right">
+            <div className="row cartTotals">
+                <div className="col-10 mt-2 ml-sm-5 col-sm-8 text-capitalize text-center">
+                    <h3 style={{background:"#DBDBDB"}} className="px-3 py-1">Your Bill</h3>
                     <Link to="/"><button className="btn btn-outline-danger text-upercase mb-3 px-5" 
                         type="button" onClick={()=>clearCart()}>Clear Cart</button>
                     </Link>
-                    <h5><span className="text-title"> subtotal :</span>{" "}
-                        <strong>$ {cartSubTotal} </strong>
-                    </h5>
-                    <h5><span className="text-title"> tax :</span>{" "}
-                        <strong>$ {cartTax} </strong>
-                    </h5>
-                    <h5><span className="text-title"> total :</span>{" "}
-                        <strong>$ {cartTotal} </strong>
-                    </h5>
-                    <PayPalButton 
+                    <div style={{background:"#DBDBDB"}} className="px-3 py-2 mb-3">
+                        <h5 className="cart-price"><span> subtotal :</span>{" "}
+                            <strong>$ {cartSubTotal} </strong>
+                        </h5>
+                        <h5 className="cart-price"><span> tax :</span>{" "}
+                            <strong>$ {cartTax} </strong>
+                        </h5>
+                        <h5 className="cart-price"><span> total :</span>{" "}
+                            <strong>$ {cartTotal} </strong>
+                        </h5>
+                    </div>
+                    <PayPalButton
                         total={cartTotal}
                         clearCart={clearCart}
                         history={history}>
